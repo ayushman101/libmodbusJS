2e1b705aaa990cc285350c7eee48daf3ec5d2a1d
ayush-antares <ayushman@antares-tech.com>
2025-06-05 07:01:04 +0000

    import statements update

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
+// const { Modbus } = require ('./build/Release/modbusJS');
+
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+
+// Use require for the native addon
 const { Modbus } = require ('./build/Release/modbusJS');
+
 
 try {
 	const modbus = new Modbus ("tcp","127.0.0.1",8502);
 	console.log (modbus.connect());
-	console.log (modbus.setSlave (2));
+	console.log (modbus.setSlave (1));
 	console.log ('Modubs slave ID: ', modbus.getSlave ());
 	console.log ('writeRegister: ', modbus.writeRegister (1, 10));
 	console.log ('writeRegisters: ', modbus.writeRegisters (2, 3, [12, 15, 23]));
