9a90c98a772227d5cb745116f266a1e4978ffb02
ayush-antares <ayushman@antares-tech.com>
2025-06-14 11:19:30 +0000

    try 5

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 
 // Use require for the native addon
-const { Modbus } = require (`./lib/binding/${process.platform}-${process.arch}/libmodbusjs.node`);
+const { Modbus } = require (`./lib/binding/${process.platform}-${process.arch}/modbusJS.node`);
 export default Modbus;
