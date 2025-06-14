400be4faafb2e7811217db596f866a418eeaae0b
ayush-antares <ayushman@antares-tech.com>
2025-06-14 11:02:48 +0000

    testing github release

--- index.js
+++ index.js
@@ -1,21 +1,6 @@
-// const { Modbus } = require ('./build/Release/modbusJS');
-
 import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 
 // Use require for the native addon
-const { Modbus } = require ('./build/Release/modbusJS');
-
-
-try {
-	const modbus = new Modbus ("tcp","127.0.0.1",8502);
-	console.log (modbus.connect());
-	console.log (modbus.setSlave (1));
-	console.log ('Modubs slave ID: ', modbus.getSlave ());
-	console.log ('writeRegister: ', modbus.writeRegister (1, 10));
-	console.log ('writeRegisters: ', modbus.writeRegisters (2, 3, [12, 15, 23]));
-	console.log ('readRegisters: ', modbus.readRegisters (1, 5));
-	modbus.close ();
-} catch (error) {
-	console.error (error);
-}
+const { Modbus } = require (`./lib/binding/${process.platform}-${process.arch}/libmodbusjs.node`);
+export default Modbus;
